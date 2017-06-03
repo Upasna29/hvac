98f0185410a11e9f44e0b79b74a9b12bfc13bd8d
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-06-03 13:38:24 -0500

    created two concurrent threads with a shared mutex

--- sensorOutput.py
+++ sensorOutput.py
@@ -1,36 +1,62 @@
 from env import environment
 from firebase import firebase
+from threading import Thread, Lock
 import serial
 import json
-import requests
 import time
-import uuid
 
 ser = serial.Serial(environment["SERIAL_PORT"], baudrate = 9600)
 firebase_url = environment["DATABASE_URL"]
 firebase = firebase.FirebaseApplication(firebase_url, None)
 fixed_interval = 10
+mutex = Lock()
+data = ""
 
-while 1:
-    data = ser.readline()
-    print data
-    temp, lux = data.split()
-    temp = float(temp)
-    lux = float(lux)
-    print 'temp: ', temp
-    print 'lux: ', lux
-    location = '/room-1'
-    data = {'temperature':temp, 'luminosity': lux}
+def write_data():
+    while True:
+        global data
+        mutex.acquire(0) # non-blocking
+        try:
+            data = ser.readline()
+        finally:
+            mutex.release()
 
-    try:
-        response = firebase.get(location, None)
-        if response:
-            key, value = response.popitem()
-            firebase.delete(location, key)
-        
-        result = firebase.post(location, data)
-        print result                
-    except Exception as e:
-        print e
+def update_db():
+    while True:
+        global data 
+        mutex.acquire(0)
+        try:
+            print "Thread 2:", data
+            if data: 
+                temp, lux = data.split()
+                temp = float(temp)
+                lux = float(lux)
+                print 'temp: ', temp
+                print 'lux: ', lux
+                location = '/room-1'
+                data = {'temperature':temp, 'luminosity': lux}
 
-    time.sleep(fixed_interval)
+                try:
+                    response = firebase.get(location, None)
+                    if response:
+                        key, value = response.popitem()
+                        firebase.delete(location, key)
+                    
+                    result = firebase.post(location, data)
+                    print result                
+                except Exception as e:
+                    print e
+
+                time.sleep(fixed_interval)
+        finally:
+            mutex.release()
+
+if __name__ == "__main__":
+    t1 = Thread(target = write_data)
+    t2 = Thread(target = update_db)
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t1.start()
+    t2.start()
+    while True:
+        pass
